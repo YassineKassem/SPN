048bf3db53465f9644d3a6be6cedcb1ef0e539ca
YassineKassem <yassinekassem33@gmail.com>
2024-02-25 12:45:14 +0100

    money transactions stat

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
 import pandas as pd
+import re 
 
 app = Flask(__name__)
 
+# Function to extract the numeric part of the amount
+def extract_numeric_amount(amount):
+    numeric_part = re.search(r'\d+\.?\d*', amount)
+    if numeric_part:
+        return float(numeric_part.group().replace(',', '.'))
+    else:
+        return 0
+    
 @app.route('/')
 def index():
     # Charger les données depuis un fichier Excel
@@ -13,8 +22,26 @@ def index():
 
     # Convertir les données en JSON
     car_owner_counts_json = car_owner_counts.to_json()
+    ######################################################
+    # Filter rows with Euro currency
+    euro_df = df[df['spnTotal amount'].notna() & df['spnTotal amount'].str.contains('€', na=False)]
+    
+    # Calculate total amount in Euro
+    total_amount_eur = sum(euro_df['spnTotal amount'].apply(lambda x: extract_numeric_amount(x)))
+    ######################################################
+    # Filter rows with Dollar currency
+    dollar_df = df[df['spnTotal amount'].notna() & df['spnTotal amount'].str.contains('\$', na=False)]
+    
+    # Calculate total amount in Dollar
+    total_amount_usd = sum(dollar_df['spnTotal amount'].apply(lambda x: extract_numeric_amount(x)))
+    ######################################################
+    # Filter rows with Pound currency
+    pound_df = df[df['spnTotal amount'].notna() & df['spnTotal amount'].str.contains('GBP|£', na=False, regex=True)]
+    
+    # Calculate total amount in Pound
+    total_amount_gbp = sum(pound_df['spnTotal amount'].apply(lambda x: extract_numeric_amount(x)))
 
-    return render_template('index.html', car_owner_counts_json=car_owner_counts_json)
+    return render_template('jdida.html', car_owner_counts_json=car_owner_counts_json,total_amount_eur=total_amount_eur,total_amount_usd=total_amount_usd,total_amount_gbp=total_amount_gbp)
 
 if __name__ == '__main__':
     app.run(debug=True)
